eec5a5ae30d7e3e519046f378ae54ea4e70817c4
George Scharps <gscharps@vt.edu>
2020-07-03 21:38:55 -0400

    adding new file stopWatch

---
+++ stopWatch.py
@@ -0,0 +1,32 @@
+#!
+#stopwatch.py - A simple stopwatch program.
+
+import time
+
+#Display the program's instructions.
+print('press enter to begin. Afterwards, press enter to "click" the stop watch')
+print('press Ctrl-C to quit.')
+
+input() #press Enter to begin
+print('Started.')
+
+startTime = time.time() # get the first lap's start time
+lastTime = startTime
+
+lapNum = 1
+
+#TODO: Start tracking the lap times.
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' %(lapNum, totalTime, lapTime), end='')
+        #end='' avoids a \n being added automatically
+        lapNum += 1
+        lastTime = time.time() #reset the last lap time
+
+
+except KeyboardInterrupt:
+    #handle the Ctrl-C exception to keep its error message from displaying.
+    print('\nDone.')
